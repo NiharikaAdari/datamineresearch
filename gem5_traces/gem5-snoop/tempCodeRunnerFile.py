b6dae05cef12ae6262f32dffd32147c2a03934a0
niha <adari.niharika13@gmail.com>
2024-06-17 02:04:21 -0400

    new file:   gem5_traces/gem5-snoop/functions.py 	modified:   gem5_traces/gem5-snoop/tempCodeRunnerFile.py 	new file:   gem5_traces/gem5-snoop/unslicedtrace-1 copy (testing)/test copy.txt 	new file:   gem5_traces/gem5-snoop/unslicedtrace-1 copy (testing)/test.txt 	modified:   gem5_traces/gem5-snoop/unslicedtrace-1 copy (testing)/unsliced-cpu0-icache0.txt 	new file:   gem5_traces/gem5-snoop/unslicedtrace-1-patterns/Patterns_AcceptanceRatios.txt 	new file:   reports/6-17 report.docx 	new file:   reports/~$17 report.docx

--- gem5_traces/gem5-snoop/tempCodeRunnerFile.py
+++ gem5_traces/gem5-snoop/tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
 
-        pairs_to_remove = []
+    trace = read_trace_from_file('gem5_traces/gem5-snoop/unslicedtrace-1 copy (testing)/unsliced-cpu0-icache0.txt')
+    pattern = [0,9]
+    result = remove_pattern_from_trace(trace, pattern)
+    print(result)
